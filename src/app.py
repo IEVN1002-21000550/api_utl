2507377a2107f91c4a5bf2316a938ea219ecd3ec
Miguel Angel Gonzalez Funes <21000550@alumnos.utleon.edu.mx>
2024-10-30 20:51:35 -0600

    insertar

--- src/app.py
+++ src/app.py
@@ -25,8 +25,54 @@ def lista_alumnos():
     except Exception as ex:
         return jsonify ({"message":"Error al conectar la base de datos {}".format(ex),'exito':False})
     
+def leer_alumno_bd(matricula):
+    try:
+        cursor=con.connection.cursor()
+        sql='select * from alumnos where matricula={0}'.format(matricula) 
+        cursor.execute(sql)
+        datos=cursor.fetchone()
+        if datos!=None:
+            alumno={'matricula':datos[0],'nombre':datos[1],'apaterno':datos[2],'amaterno':datos[3],'correo':datos[4]}
+            return alumno
+        else:
+            return None
+    except Exception as ex:
+        return jsonify ({"message":"Error al conectar la base de datos {}"})
 
 
+
+
+
+
+@app.route("/alumnos/<mat>", methods=["GET"])
+def leer_alumno(mat):
+    try:
+        alumno=leer_alumno_bd(mat)
+        if alumno!=None:
+            return jsonify({'alumno':alumno,'mensaje':'Alumno encontrado', 'exito':True})
+        else:
+            return jsonify({'mensaje':'Alumno no encontrado', 'exito':False})
+    except Exception as ex:
+        return jsonify ({"message":"Error al conectar la base de datos {}".format(ex),'exito':False})
+
+@app.route("/alumnos", methods=["POST"])
+def registrar_alumno():
+    try:
+        alumno=leer_alumno_bd(request.json['matricula'])
+        if alumno!=None:
+            return jsonify({'mensaje':'Alumno ya existe', 'exito':False})
+        else:
+            cursor=con.connection.cursor()
+            sql='''insert into alumnos (matricula,nombre,apaterno,amaterno,correo)
+                values('{0}','{1}','{2}','{3}','{4}')'''. format(request.json['matricula'],
+                request.json['nombre'],request.json['apaterno'],request.json['amaterno'],
+                request.json['correo'],)           
+            cursor.execute(sql)
+            con.connection.commit() #para guardar los cambios en la base de datos            
+            return jsonify({'mensaje':'Alumno Agregado', 'exito':True})
+    except Exception as ex:
+        return jsonify ({"message":"Error al conectar la base de datos {}".format(ex),'exito':False})
+    
 def pagina_no_encontrada(error):
     return "<h1>la pagina que estas buscando no existe</h1>",400
 if __name__=="__main__":
